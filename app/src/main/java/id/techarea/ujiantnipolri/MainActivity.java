97f20959045a67c7dd29c947fae6a6d299a39c7f
fachruddin <imandafachruddin7@gmail.com>
2019-03-08 14:31:45 +0700

    add

--- app/src/main/java/id/techarea/ujiantnipolri/MainActivity.java
+++ app/src/main/java/id/techarea/ujiantnipolri/MainActivity.java
@@ -1,7 +1,11 @@
 package id.techarea.ujiantnipolri;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
@@ -11,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static int Splash_Time_Out = 3000;
 
+    public static final int requestCode = 200;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,13 +25,70 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent appIntent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(appIntent);
-                finish();
+        askPermissions();
+
+    }
+
+    protected void askPermissions() {
+        String[] permissions = {
+                "android.permission.READ_EXTERNAL_STORAGE",
+                "android.permission.WRITE_EXTERNAL_STORAGE",
+                "android.permission.INTERNET",
+                "android.permission.ACCESS_NETWORK_STATE"
+        };
+
+        int permissionCheckInternalStorage = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int permissionReadStorage = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        int permissionInternet = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.INTERNET);
+        int permissionNetwork = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_NETWORK_STATE);
+
+        // Here, thisActivity is the current activity
+        if (permissionCheckInternalStorage != PackageManager.PERMISSION_GRANTED ||
+                permissionReadStorage != PackageManager.PERMISSION_GRANTED||
+                permissionInternet != PackageManager.PERMISSION_GRANTED||
+                permissionNetwork != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    permissions,
+                    requestCode);
+        }
+        else{
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent appIntent = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(appIntent);
+                    finish();
+                }
+            },Splash_Time_Out);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        if(this.requestCode == requestCode) {
+            // If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                // permission was granted, yay! Do the
+                // contacts-related task you need to do.
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent appIntent = new Intent(MainActivity.this, LoginActivity.class);
+                        startActivity(appIntent);
+                        finish();
+                    }
+                },Splash_Time_Out);
+
+
+            } else {
+                askPermissions();
             }
-        },Splash_Time_Out);
+            return;
+        }
     }
 }
--- app/src/main/java/id/techarea/ujiantnipolri/PilihUjianActivity.java
+++ app/src/main/java/id/techarea/ujiantnipolri/PilihUjianActivity.java
@@ -1,13 +1,17 @@
 package id.techarea.ujiantnipolri;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
-public class PilihUjianActivity extends AppCompatActivity {
+public class PilihUjianActivity extends AppCompatActivity implements View.OnClickListener {
+
+    LinearLayout btn_bahasa_inggris, btn_bahasa_indonesia,  btn_pengetahuan_umum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +21,37 @@ public class PilihUjianActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_pilih_ujian);
 
+        btn_bahasa_inggris = (LinearLayout) findViewById(R.id.btn_bahasa_inggris);
+        btn_bahasa_indonesia = (LinearLayout) findViewById(R.id.btn_bahasa_indonesia);
+        btn_pengetahuan_umum = (LinearLayout) findViewById(R.id.btn_pengetahuan_umum);
 
+        btn_bahasa_inggris.setOnClickListener(this);
+        btn_bahasa_indonesia.setOnClickListener(this);
+        btn_pengetahuan_umum.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case (R.id.btn_bahasa_inggris):
+                final Intent bahasa_inggris = new Intent(PilihUjianActivity.this, SoalActivity.class);
+                bahasa_inggris.putExtras(getIntent().getExtras());
+                startActivity(bahasa_inggris);
+                break;
+
+            case (R.id.btn_bahasa_indonesia):
+                final Intent bahasa_indonesia = new Intent(PilihUjianActivity.this, SoalActivity.class);
+                bahasa_indonesia.putExtras(getIntent().getExtras());
+                startActivity(bahasa_indonesia);
+                break;
+
+            case (R.id.btn_pengetahuan_umum):
+                final Intent pengetahuan_umum = new Intent(PilihUjianActivity.this, SoalActivity.class);
+                pengetahuan_umum.putExtras(getIntent().getExtras());
+                startActivity(pengetahuan_umum);
+                break;
+        }
     }
 
 }
